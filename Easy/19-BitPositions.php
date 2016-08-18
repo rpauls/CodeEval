522a487148d693f21073cee68af9a82a7e4115ac
Robert K. Pauls <rpauls@users.noreply.github.com>
2016-08-18 12:19:27 +0200

    Code optimized;

--- Easy/19-BitPositions.php
+++ Easy/19-BitPositions.php
@@ -2,24 +2,19 @@
 /*
  *  2016-08-12
  *  CodeEval Challenge - Easy
- *  bit Positions
+ *  Bit Positions
  */
 
 $input = fopen($argv[1], 'r');
 
 while (!feof($input)) {
-    $trimmed= trim(fgets($input));
-    $exploded = explode(',', $trimmed);
-    $bit = strrev(decbin($exploded[0]));
-    $bit_split = str_split($bit);
+    $exploded = explode(',', trim( fgets($input)) );
+    $bit_split = str_split( strrev(decbin($exploded[0])) );
 
-    if($bit_split[$exploded[1]-1] == $bit_split[$exploded[2]-1]) {
-        echo 'true'. "\n";
+    if ($bit_split[$exploded[1]-1] == $bit_split[$exploded[2]-1]) {
+        echo 'true'. PHP_EOL;
     } else {
-        echo 'false'. "\n";
+        echo 'false'. PHP_EOL;
     }
-
 }
 fclose($input);
-
-?>
