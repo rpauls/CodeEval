fca937215570e589ac532107029238fa81ac8511
Robert K. Pauls <rpauls@users.noreply.github.com>
2016-08-18 12:45:19 +0200

    Reduced If-Statement to short version and some minor style changes;

--- Easy/31-RightmostChar.php
+++ Easy/31-RightmostChar.php
@@ -7,19 +7,10 @@
  */
 
 $input = fopen($argv[1], 'r');
-// $trimmed = 'L0S6gbOz6uVlv3pVislw7KFmDxoWRbHql8V8N6G2 T3gt9fpRL2Psf2ypyAinWDQhBVlwW,o';
 while (!feof($input)) {
-    $trimmed= trim(fgets($input));
-    $exploded = explode(',', $trimmed);
-
+    $exploded = explode(',', trim(fgets($input)) );
     $strPos = strrpos(trim($exploded[0]),trim($exploded[1]));
 
-    if($strPos <= 0) {
-        $strPos = -1;
-    }
-
-    echo $strPos . "\n";
-
+    echo (($strPos <= 0) ? -1 : $strPos) . PHP_EOL;
 }
 fclose($input);
-?>
