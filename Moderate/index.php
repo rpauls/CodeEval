c5ef933d8e5a55b0595dc9e8386891a210fe05f2
Robert K. Pauls <r.pauls@robertkpauls.com>
2016-08-26 11:43:00 +0200

    Organized directory structure of moderate challenges

--- Moderate/index.php
+++ Moderate/index.php
@@ -53,8 +53,7 @@
     if ($handle = opendir('.')) {
 
         while (false !== ($entry = readdir($handle))) {
-
-            if ($entry != '.' && $entry != '..' && $entry != 'index.php') {
+            if ($entry != '.' && $entry != '..' && $entry != 'index.php' && $entry != '.editorconfig') {
                 echo '<a href="'.$entry.'">'.$entry.'</a>';
             }
         }
