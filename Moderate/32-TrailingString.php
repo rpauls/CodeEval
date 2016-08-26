ed27faebbdd8330039631275300166dab5d126c6
Robert K. Pauls <r.pauls@robertkpauls.com>
2016-08-26 11:36:02 +0200

    Finished Chalange 32 - Trailing String

--- Moderate/32-TrailingString.php
+++ Moderate/32-TrailingString.php
@@ -5,28 +5,13 @@
  *  Trailing String
  */
 
-$input = 'San Francisco,San Jose';
+$input = fopen($argv[1], 'r');
+while (!feof($input)) {
+    $expl = explode(',', trim(fgets($input)));
 
-// $input = fopen($argv[1], 'r');
-// while (!feof($input)) {
-    // $expl = explode(',', trim(fgets($input)));
-    $expl = explode(',', trim($input));
+    $s = strpos($expl[0], $expl[1]);
+    $l = $s + strlen($expl[1]);
 
-    var_dump($expl[0]);     // DEBUG
-    var_dump($expl[1]);     // DEBUG
-    echo '<br/>';           // DEBUG
-
-    $trail = strrev($expl[1]);
-    $cTrail = strlen($expl[1]);
-
-
-    $str = substr(strrev($expl[0]), 0, $cTrail);
-
-
-    var_dump($str);         // DEBUG
-    var_dump($trail);       // DEBUG
-    echo '<br/>';           // DEBUG
-
-    echo $trail === $str ? '1' : '0'. PHP_EOL;
-// }
-// fclose($input);
+    echo (($s !== false && $l === strlen($expl[0])) ? '1' : '0') . PHP_EOL;
+}
+fclose($input);
