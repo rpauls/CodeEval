b46a4f26ba7069ac8420a0379194c706f80910c6
Robert K. Pauls <r.pauls@robertkpauls.com>
2016-09-05 14:45:39 +0200

    Added solution for challenge 85

--- Hard/085-FindMin/085.php
+++ Hard/085-FindMin/085.php
@@ -3,19 +3,37 @@
  *  2016-08-30
  *  CodeEval Challenge - Hard
  *  Find Min
+ *  https://www.codeeval.com/browse/85/
  */
 
 // DEBUG
-$input = fopen('./input.txt', 'r');
+// $input = fopen('./input.txt', 'r');
 // PROD
-// $input = fopen($argv[1], 'r');
+$input = fopen($argv[1], 'r');
 while (!feof($input)) {
-    $expl = explode(',', trim(fgets($input)));
-    $x = 0;
+    list($n,$k,$a,$b,$c,$r)  = explode(',', trim(fgets($input)));
+    $m = array();
 
+    $m[0] = $a;
+    for ($i=1; $i < $k ; $i++) {
+        $m[] = ($b * $m[$i-1] + $c) % $r;
+    }
 
+    for ($i = $k; $i < $n ; $i++) {
 
-    echo $x . PHP_EOL;
+        $s = array_slice($m, $i-$k, $k);
+        $s = array_flip($s);
 
+        $min = 0;
+
+        while (isset($s[$min])) {
+            $min++;
+        }
+
+        $m[] = $min;
+
+    }
+
+    echo $m[$n-1] . PHP_EOL;
 }
 fclose($input);
